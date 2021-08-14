5b0fbef2b9faf399b56d9190c3355176dfd111d1
sambasile10 <sbasile2019@gmail.com>
2021-08-13 18:09:37 -0600

    start of twitch client

--- src/Client/TwitchClient.ts
+++ src/Client/TwitchClient.ts
@@ -1,20 +1,40 @@
-import tmi from 'tmi.js'
+import * as tmi from 'tmi.js'
 
-const client = new tmi.Client({
-    options: {debug: true },
-    connection: {
-        secure: true,
-        reconnect: true
-    },
-    identity: {
-        username: "placeholderlol",
-        password: 'don\'t put a token here :) '
-    },
-    channels: [ 'sodapoppin' ]
-});
+export class TwitchClient {
 
-client.connect();
+    private client: tmi.Client;
+    private channelList: string[];
 
-client.on('message', (channel, tags, message, self) => {
-    if(self) { return; }
-});
+    constructor(channelList: string[]) {
+        this.channelList = channelList;
+        this.client = new tmi.Client({
+            options: { debug: true },
+            connection: {
+                secure: true,
+                reconnect: true
+            },
+            //TODO identity
+            channels: channelList
+        });
+    }
+
+    protected async start(): Promise<void> {
+        return await this.connect();
+    }
+
+    private connect(): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.client.connect().then((res) => {
+                resolve();
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+    }
+
+    private listen(): void {
+        this.client.on("message", (channel, userstate, message, self) => {
+            if(self) { return; }
+        });
+    }
+}
